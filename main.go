96753c22b9a9641f92f707e961a5d8314a2cbfe3
Jason Song <i@wolfogre.com>
2019-05-04 17:50:08 +0800

    style: add comments
    Change-Id: Ifec551b6ae0a3cdae861af67b968d44ee7ef5961

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"github.com/mholt/caddy/caddy/caddymain"
 	// plug in plugins here, for example:
 	// _ "import/path/here"
-	_ "github.com/aablinov/caddy-geoip"
-	_ "github.com/epicagency/caddy-expires"
-	_ "github.com/nicolasazrak/caddy-cache"
+	_ "github.com/aablinov/caddy-geoip"     // http.genip
+	_ "github.com/epicagency/caddy-expires" // http.expire
+	_ "github.com/nicolasazrak/caddy-cache" // http.cache
 )
 
 func main() {
