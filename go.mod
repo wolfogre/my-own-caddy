499fbce9f42ddace1d2e86b0e2d5763cb1ebad7a
Jason Song <i@wolfogre.com>
2019-08-13 21:58:21 +0800

    feat: use my own http.prometheus
    Change-Id: Ie489c742b8576cf2f670b12424555610b3808ace

--- go.mod
+++ go.mod
@@ -6,9 +6,9 @@ require (
 	github.com/aablinov/caddy-geoip v0.0.0-20190710083220-6705babb56ca
 	github.com/caddyserver/caddy v1.0.1
 	github.com/epicagency/caddy-expires v1.1.1
-	github.com/miekg/caddy-prometheus v0.0.0-20190709133612-1fe4cb19becd
 	github.com/mmcloughlin/geohash v0.9.0 // indirect
 	github.com/nicolasazrak/caddy-cache v0.3.4
 	github.com/oschwald/maxminddb-golang v1.3.1 // indirect
 	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
+	github.com/wolfogre/caddy-prometheus v1.0.0
 )
--- main.go
+++ main.go
@@ -4,10 +4,10 @@ import (
 	"github.com/caddyserver/caddy/caddy/caddymain"
 	// plug in plugins here, for example:
 	// _ "import/path/here"
-	_ "github.com/aablinov/caddy-geoip"     // http.genip
-	_ "github.com/epicagency/caddy-expires" // http.expire
-	_ "github.com/miekg/caddy-prometheus"   // http.prometheus
-	_ "github.com/nicolasazrak/caddy-cache" // http.cache
+	_ "github.com/aablinov/caddy-geoip"      // http.genip
+	_ "github.com/epicagency/caddy-expires"  // http.expire
+	_ "github.com/nicolasazrak/caddy-cache"  // http.cache
+	_ "github.com/wolfogre/caddy-prometheus" // http.prometheus
 )
 
 func main() {
